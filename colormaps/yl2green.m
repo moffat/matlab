9f27b2eccbc18b7b09508b8946275ba72dd4da78
Carlos Moffat <carlos@moffat.cl>
2015-07-10 09:15:17 -0700

    Add colormap folder.

---
+++ colormaps/yl2green.m
@@ -0,0 +1,26 @@
+function cm = yl2green(n)
+
+% by Carlos Moffat (carlos.moffat@ucsc.edu)
+% Created on:   2015-07-09 21:10:26
+% Last Modified: 2015-07-09 21:38:49
+
+if nargin == 0
+    n = 64;
+end
+
+rgb = [255 255 229;...
+       247 252 185;...
+       217 240 163;...
+       173 221 142;...
+       120 198 121;...
+        65 171  93;...
+        35 132  67;...
+         0 104  55;...
+         0  69  41]./255;
+
+
+nn = numel(rgb(:))/3-1;
+nn = ((0:nn)./nn)';
+
+n = ((0:n-1))./(n-1);
+cm = interp1(nn,rgb,n);
